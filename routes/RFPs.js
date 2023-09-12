6a23adc4d3f43a24c2c5bec922edfe9a6ec205b4
saman-alayee <saman.alaii10@gmail.com>
2023-09-12 18:02:55 +0330

    rfp

--- routes/RFPs.js
+++ routes/RFPs.js
@@ -16,9 +16,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-router.get("/", auth, async (req, res) => {
-  const requests = await Request.find().sort("name");
-  res.send(requests);
+router.get("/",  async (req, res) => {
+  const rfps = await RFP.find().sort("name");
+  res.send(rfps);
 });
 
 router.post("/", async (req, res) => {
@@ -26,42 +26,76 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   // Create a new Request instance
-  let request = new Request({
-    // Set properties based on the request body
-    // (Assuming your request schema has properties like firstName, lastName, phone, email, etc.)
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    phone: req.body.phone,
-    email: req.body.email,
-    service: req.body.service,
-    company: req.body.company,
-    domain: req.body.domain,
-    description: req.body.description,
-    // ... other properties
-  });
+  let rfp = new RFP({
+    field_1: req.body.field_1,
+    field_2: req.body.field_2,
+    field_3: req.body.field_3,
+    field_4: req.body.field_4,
+    field_5: req.body.field_5,
+    field_6: req.body.field_6,
+    field_7: req.body.field_7,
+    field_8: req.body.field_8,
+    field_9: req.body.field_9,
+    field_10: req.body.field_10,
+    field_11: req.body.field_11,
+    field_12: req.body.field_12,
+    field_13: req.body.field_13,
+    field_14: req.body.field_14,
+    field_15: req.body.field_15,
+    field_16: req.body.field_16,
+    field_17: req.body.field_17,
+    field_18: req.body.field_18,
+    field_19: req.body.field_19,
+    field_20: req.body.field_20,
+    field_21: req.body.field_21,
+    field_22: req.body.field_22,
+    field_23: req.body.field_23,
+    field_24: req.body.field_24,
+    field_25: req.body.field_25,
+    field_26: req.body.field_26,
+    field_27: req.body.field_27,
+    field_28: req.body.field_28,
+    field_29: req.body.field_29,
+    field_30: req.body.field_30,
+    field_31: req.body.field_31,
+    field_32: req.body.field_32,
+    field_33: req.body.field_33,
+    field_34: req.body.field_34,
+    field_35: req.body.field_35,
+    field_36: req.body.field_36,
+    field_37: req.body.field_37,
+    field_38: req.body.field_38,
+    field_39: req.body.field_39,
+    field_40: req.body.field_40,
+    field_41: req.body.field_41,
+    field_42: req.body.field_42,
+    field_43: req.body.field_43,
+    field_44: req.body.field_44
+});
 
-  // Save the new request to the database
-  request = await request.save();
 
-  // Send the newly created request as the response
-  res.send(request);
+  // Save the new question to the database
+  rfp = await rfp.save();
+
+  // Send the newly created question as the response
+  res.send(rfp);
 });
 // ... (previous imports and code)
 
 router.delete("/:id", auth, async (req, res) => {
   try {
-    // Find the request by ID and delete it
-    const request = await Request.findByIdAndRemove(req.params.id);
+    // Find the question by ID and delete it
+    const rfp = await RFP.findByIdAndRemove(req.params.id);
 
-    if (!request) {
-      return res.status(404).send("Request with the given ID was not found.");
+    if (!rfp) {
+      return res.status(404).send("question with the given ID was not found.");
     }
 
-    res.send(request);
+    res.send(rfp);
   } catch (error) {
     return res
       .status(500)
-      .send("An error occurred while deleting the request.");
+      .send("An error occurred while deleting the question.");
   }
 });
 
@@ -89,26 +123,26 @@ router.delete("/:id", auth, async (req, res) => {
 router.delete("/", auth, async (req, res) => {
   try {
     // Delete all requests
-    const result = await Request.deleteMany();
+    const result = await RFP.deleteMany();
 
     if (result.deletedCount === 0) {
-      return res.status(404).send("No requests found to delete.");
+      return res.status(404).send("No rfp found to delete.");
     }
 
-    res.send(`Deleted ${result.deletedCount} requests.`);
+    res.send(`Deleted ${result.deletedCount} rfps.`);
   } catch (error) {
-    return res.status(500).send("An error occurred while deleting requests.");
+    return res.status(500).send("An error occurred while deleting rfps.");
   }
 });
 
 router.get("/exportToExcel", auth, async (req, res) => {
   try {
-    // Fetch all requests from the database
-    const requests = await Request.find();
+    // Fetch all questions from the database
+    const rfps = await RFP.find();
 
     // Create a new Excel workbook and worksheet
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet("Requests");
+    const worksheet = workbook.addWorksheet("Questions");
 
     // Define headers for the Excel file
     const headers = [
--- startup/routes.js
+++ startup/routes.js
@@ -4,6 +4,8 @@ var demoRequest = require("../routes/request");
 var demoAuth = require("../routes/auth");
 var demoUser = require("../routes/user");
 var demoTicket = require("../routes/tickets");
+var demoRFPs = require("../routes/RFPs");
+
 
 module.exports = function (app) {
   app.use(express.json());
@@ -12,5 +14,6 @@ module.exports = function (app) {
   app.use("/api/auth", demoAuth);
   app.use("/api/users", demoUser);
   app.use("/api/ticket", demoTicket);
+  app.use("/api/rfp",demoRFPs)
 
 };
