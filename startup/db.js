060e7efc9742e87f45ed70e4bf603c1d9ad1a0c4
saman-alayee <saman.alaii10@gmail.com>
2024-09-01 14:35:16 +0330

    deploy

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,25 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
+// Configure winston
+winston.configure({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
 module.exports = function() {
   const mongoURI = config.get("mongo_URI");
-  mongoose.connect(mongoURI)
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
     .then(() => winston.info('Connected to MongoDB...'))
-    .catch((err)=> console.log(err))
+    .catch((err) => winston.error('Error connecting to MongoDB:', err));
 }
